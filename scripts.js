79d3aa0ed99e95729586596c0add0c77ddeb7f16
justin36070 <66752121+justin36070@users.noreply.github.com>
2023-10-08 16:40:36 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,3 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     // Add any dynamic JS functionalities here, like fetching data from an API or handling form submissions.
     //document.getElementById("video1").currentTime = 10;
 });
+
+function Menu() {
+    window.scrollBy(0,-100)
+    
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
