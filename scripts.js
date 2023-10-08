e2c0d3791880b27194cce85a340bcbac326130c5
justin36070 <66752121+justin36070@users.noreply.github.com>
2023-10-08 02:39:34 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Add any dynamic JS functionalities here, like fetching data from an API or handling form submissions.
+    //document.getElementById("video1").currentTime = 10;
 });
